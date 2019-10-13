c5bc9327809fde501aff5f8ae9c23aece17996a6
David Bermudez <juan.bermudez1102@outlook.com>
2019-10-13 00:28:20 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,13 @@
-# Your code goes here!
+class Dog
+  def name=(the_dog_name)
+    @the_dog_name = the_dog_name
+  end
+
+  def name
+    @the_dog_name
+  end
+
+  def bark
+    puts "woof!"
+  end
+end
